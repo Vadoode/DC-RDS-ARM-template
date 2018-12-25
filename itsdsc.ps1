5b59b382aea3e26b65249b83eb2d1105bdf734e7
Vadood <vadood.e@gmail.com>
2018-12-25 15:39:45 +1100

    initial creation

---
+++ itsdsc.ps1
@@ -0,0 +1,55 @@
+﻿Configuration ITSwitchDC {
+
+ param ( 
+        $CountryName, $PsgrName
+    )
+
+    # this module contains the resources we're using.
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+     @('AD-Domain-Services', 'DNS', 'GPMC', 'RSAT-AD-Tools', 'RSAT-DNS-Server','RSAT-ADDS').ForEach({
+             WindowsFeature $_
+             {
+                Name = $_
+                Ensure = 'Present'
+            }
+         } 
+      )
+   
+
+    File TestFile
+    {
+        Ensure = "Present"
+        DestinationPath = "C:\TestFile"
+        Contents = "Hello there $PsgrName! Welcome to $CountryName!"
+    }
+}
+
+
+Configuration ITSwitchRDS {
+
+    # this module contains the resources we're using.
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+  
+   
+     @('RDS-RD-Server', 'Remote-Desktop-Services', 'RSAT-RDS-Tools', 'RDS-Connection-Broker','RDS-Web-Access', 'RDS-Licensing').ForEach({
+             WindowsFeature $_
+             {
+                Name = $_
+                Ensure = 'Present'
+            }
+         } 
+      )
+   
+    File TestFile
+    {
+        Ensure = "Present"
+        DestinationPath = "C:\TestFile"
+        Contents = "Hello there! Welcome to CountryName!"
+    }
+}
+
+
+
+
+
